501959089f398219b4edb2aa3c9713f5662b1458
Maruan Vicente Cardenas El Hachmaoui <maruan3d@gmail.com>
2022-06-08 13:53:28 +0200

    added copyright symbol

--- components/Footer/index.jsx
+++ components/Footer/index.jsx
@@ -4,7 +4,7 @@ import styles from "./styles.module.scss";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div>Copyrigth Maruan Vicente Cardenas El Hachmaoui</div>
+      <div>Copyrigth &copy; Maruan Vicente Cardenas El Hachmaoui</div>
     </footer>
   );
 };
